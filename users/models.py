4920cc5ce076c0d89d20b9d4f8b4692b356f2dc2
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-02-23 08:24:24 +0000

    chore: init from djereo v1.0.0-rc1 [153fee6]

---
+++ users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from testdjereo.models import UuidModel
+
+
+class AuthUser(UuidModel, AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.email
+
+
+class UserProfile(UuidModel):
+    user = models.OneToOneField(
+        "users.AuthUser",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"Profile for {self.user}"
+
+
+@receiver(post_save, sender=AuthUser)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    UserProfile.objects.update_or_create(user=instance)
