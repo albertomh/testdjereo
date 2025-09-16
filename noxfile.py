9d3ba7cf961b0c3b25ad7be16e0198045dd2ea2f
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-09-16 22:13:42 +0100

    tests: run in parallel

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ nox.options.default_venv_backend = "uv"
 nox.options.sessions = [f"tests_with_coverage-{LATEST_PY}"]
 
 run_tests_args = ["manage.py", "test"]
-run_tests_flags = ["--failfast", "--pdb", "--shuffle", "--verbosity=3"]
+base_flags = ["--failfast", "--shuffle", "--verbosity=3"]
 
 
 def _install_deps(session: nox.Session) -> None:
@@ -26,6 +26,19 @@ def _install_deps(session: nox.Session) -> None:
     session.install("-e", ".")
 
 
+def _effective_flags(session: nox.Session) -> list[str]:
+    """Manipulate Django test runner flags to avoid incompatibilities.
+
+    Parallel execution must be disabled (force DJANGO_TEST_PROCESSES to 1) if using `pdb`.
+    <https://docs.djangoproject.com/en/stable/ref/django-admin/#cmdoption-test-parallel>
+    """
+    test_processes = 2
+    if "--pdb" in session.posargs:
+        test_processes = 1
+
+    return [*base_flags, f"--parallel={test_processes}", *session.posargs]
+
+
 @nox.session(python=MIDDLE_PY)
 def tests(session: nox.Session) -> None:
     _install_deps(session)
@@ -34,8 +47,7 @@ def tests(session: nox.Session) -> None:
         "python",
         "-Wall",
         *run_tests_args,
-        *session.posargs,
-        *run_tests_flags,
+        *_effective_flags(session),
     )
 
 
@@ -50,8 +62,7 @@ def tests_with_coverage(session: nox.Session) -> None:
         "--source",
         ".",
         *run_tests_args,
-        *session.posargs,
-        *run_tests_flags,
+        *_effective_flags(session),
     )
     session.run("coverage", "report")
     session.run("coverage", "html")
